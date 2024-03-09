29542fd8934950f4f278f651a76822cfaf554a50
zainabmirza-eng <zainabmirzafatah99@gmail.com>
2024-03-09 18:39:24 +0300

    added

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+
+const currentUser = 'Grace Hopper';
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + '!';
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+
+console.log(welcomeMessage);
+console.log(excitedWelcomeMessage);
+console.log(shortGreeting);
